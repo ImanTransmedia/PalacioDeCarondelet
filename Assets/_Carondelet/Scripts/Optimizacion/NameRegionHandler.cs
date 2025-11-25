9597005803ee774bcba3b0a6332e864cbdb1f0b7
Serghy Castillo <b1uefeet.dev@gmail.com>
2025-11-25 17:32:54 -0500

    Update Titulos Banquetes

--- Assets/_Carondelet/Scripts/Optimizacion/NameRegionHandler.cs
+++ Assets/_Carondelet/Scripts/Optimizacion/NameRegionHandler.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.Localization;
 
+[RequireComponent(typeof(Collider))]
 public class NameRegionHandler : MonoBehaviour
 {
     [Header("Texto a modificar")]
@@ -9,22 +10,66 @@ public class NameRegionHandler : MonoBehaviour
 
     [Header("Strings localizados")]
     [SerializeField] private LocalizedString onEnterText; 
-    [SerializeField] private LocalizedString onExitText;  
+    [SerializeField] private LocalizedString onExitText; 
+
+    [Header("Detección de jugador")]
+    [SerializeField] private string playerTag = "Player";
+
+    private Collider triggerCollider;
 
     private void Reset()
     {
         if (targetText == null)
             targetText = GetComponent<TMP_Text>();
+
+        triggerCollider = GetComponent<Collider>();
+        triggerCollider.isTrigger = true;
+    }
+
+    private void Awake()
+    {
+        triggerCollider = GetComponent<Collider>();
+    }
+
+    private void Start()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag(playerTag);
+
+        if (player != null && triggerCollider != null)
+        {
+            Collider playerCollider = player.GetComponent<Collider>();
+
+            if (playerCollider != null)
+            {
+                bool estaDentro = triggerCollider.bounds.Intersects(playerCollider.bounds);
+
+                if (estaDentro)
+                    SetLocalizedText(onEnterText);
+                else
+                    SetLocalizedText(onExitText);
+            }
+            else
+            {
+                SetLocalizedText(onExitText);
+            }
+        }
+        else
+        {
+            SetLocalizedText(onExitText);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        if (!other.CompareTag(playerTag)) return;
 
         SetLocalizedText(onEnterText);
     }
 
     private void OnTriggerExit(Collider other)
     {
+        if (!other.CompareTag(playerTag)) return;
+
         SetLocalizedText(onExitText);
     }
 
