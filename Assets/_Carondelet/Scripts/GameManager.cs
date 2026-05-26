7bf733cc818ef032d38b5d37ecdf1c813bb62d68
Serghy Castillo <b1uefeet.dev@gmail.com>
2026-05-26 13:28:15 -0500

    Update de Media Resources

--- Assets/_Carondelet/Scripts/GameManager.cs
+++ Assets/_Carondelet/Scripts/GameManager.cs
@@ -10,12 +10,14 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
-    public static string CommonImageURL = "https://palaciocarondelet360.presidencia.gob.ec/MediaResources/Images/";
-    public static string CommonVideoURL = "https://palaciocarondelet360.presidencia.gob.ec/MediaResources/Videos/";
+    //public static string CommonImageURL = "https://palaciocarondelet360.presidencia.gob.ec/MediaResources/Images/";
+    //public static string CommonVideoURL = "https://palaciocarondelet360.presidencia.gob.ec/MediaResources/Videos/";
     // TESTING
-    //public static string ConfigurationURL = "https://historiasmagneticas.com/Carondelet/configInteractuables.json";
+    public static string ConfigurationURL = "https://historiasmagneticas.com/Carondelet/configInteractuables.json";
+    public static string CommonImageURL = "https://historiasmagneticas.com/Carondelet/MediaResources/Images/";
+    public static string CommonVideoURL = "https://historiasmagneticas.com/Carondelet/MediaResources/Videos/";
     // DEPLOY
-    public static string ConfigurationURL = "https://palaciocarondelet360.presidencia.gob.ec/configInteractuables.json";
+    //public static string ConfigurationURL = "https://palaciocarondelet360.presidencia.gob.ec/configInteractuables.json";
     public static string CommonVideoListURL = "https://historiasmagneticas.com/Carondelet/MediaResources/Videos/SalonAzul/";
 
     private const bool ALWAYS_REFRESH = true;
